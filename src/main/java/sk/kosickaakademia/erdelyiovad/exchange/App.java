9b9070a39b274f02fb67ff4f5f43430e5b39a894
Dominika Erdelyiova <dominika.erdelyiova@gmail.com>
2021-04-08 09:46:33 +0200

    Initial commit

---
+++ src/main/java/sk/kosickaakademia/erdelyiovad/exchange/App.java
@@ -0,0 +1,9 @@
+package sk.kosickaakademia.erdelyiovad.exchange;
+
+public class App {
+    public static void main(String[] args) {
+        {
+            System.out.println("Welcome to the Exchange Rates app! :)");
+        }
+    }
+}
