aa8d9270f58bb98db52d471611db193bb6e1e1c9
Dominika Erdelyiova <dominika.erdelyiova@gmail.com>
2021-04-08 10:28:05 +0200

    added ApiRequest class

---
+++ src/main/java/sk/kosickaakademia/erdelyiovad/api/ApiRequest.java
@@ -0,0 +1,17 @@
+package sk.kosickaakademia.erdelyiovad.api;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ApiRequest {
+    public Map getExchangeRates(Set<String> rates){
+
+        if(rates==null || rates.size() ==0)
+            return null;
+
+        return null;
+    }
+    private void getRatesFromAPIServer(){
+
+    }
+}
